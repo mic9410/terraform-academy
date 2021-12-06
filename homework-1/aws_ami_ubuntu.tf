5b5a5430f9ff995f9d63e92bbcd917384dba00af
Michal Foryt <michal.foryt@gmail.com>
2021-12-06 19:29:50 +0100

    nginx implemented

--- homework-1/aws_ami_ubuntu.tf
+++ homework-1/aws_ami_ubuntu.tf
@@ -20,7 +20,7 @@ resource "aws_key_pair" "id_rsa" {
 }
 
 resource "aws_instance" "app_server" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-036d46416a34a611c"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.id_rsa.key_name
   vpc_security_group_ids = [aws_security_group.main.id]
@@ -29,6 +29,9 @@ resource "aws_instance" "app_server" {
     inline = [
       "touch hello.txt",
       "echo helloworld remote provisioner >> hello.txt",
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
     ]
   }
   connection {
@@ -36,7 +39,7 @@ resource "aws_instance" "app_server" {
     host        = self.public_ip
     user        = "ubuntu"
     private_key = file("/home/michal/.ssh/id_rsa")
-    timeout     = "4m"
+    timeout     = "10m"
   }
 }
 
@@ -44,16 +47,15 @@ resource "aws_security_group" "main" {
   ingress {
     description = "SSH"
     from_port   = 0  # SSH client port is not a fixed port
-    to_port     = 22
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] #allow web traffic. 46.64.73.251/32
   }
 
   egress {
     from_port   = 0
-    to_port     = 22  # SSH client port is not a fixed port
+    to_port     = 65535  # SSH client port is not a fixed port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
